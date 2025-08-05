6b77c217601cf4fba50c8330a99389531100ffe5
quangdepchai2201 <quangdoanminh2201@gmail.com>
2025-08-05 18:27:04 +0700

    Create aya.lua

---
+++ aya.lua
@@ -0,0 +1,224 @@
+getgenv().ConfigsKaitun = {
+	["Block Pet Gift"] = true,
+
+	Collect_Cooldown = 30, -- cooldown to collect fruit
+	JustFuckingCollectAll = true, -- Collect all (fruit not wait mutation)
+
+	["Low Cpu"] = true,
+	["Auto Rejoin"] = true,
+
+	["Rejoin When Update"] = false,
+	["Limit Tree"] = {
+		["Limit"] = 100,
+		["Destroy Untill"] = 90,
+
+		["Safe Tree"] = {
+			["Tomato"] = 10,
+			["Blood Banana"] = 10,
+			["Bamboo"] = 5,
+			["Mango"] = 3,
+			["Pineapple"] = 3,
+			["Beanstalk"] = 5,
+			["Giant Pinecone"] = 5,
+			["Sugar Apple"] = 20,
+			["Pepper"] = 10,
+			["Cacao"] = 10,
+			["Elder Strawberry"] = 10,
+			["Apple"] = 5,
+			"Bone Blossom",
+			"Grand Tomato",
+			["Ember Lily"] = 10,
+			["Violet Corn"] = 3,
+			["Pumpkin"] = 7,
+			["Coconut"] = 3,
+			["Sugarglaze"] = 5,
+			["Grape"] = 5,
+			["Corn"] = 10,
+			["Banana"] = 2,
+			["Serenity"] = 1,
+			["Moon Mango"] = 1,
+			["Maple Apple"] = 10,
+		}
+	},
+
+	Seed = {
+		Buy = {
+			Mode = "Auto", -- Custom , Auto
+			Custom = { -- any fruit u need to place
+				"Carrot",
+			}
+		},
+		Place = {
+			Mode = "Select", -- Select , Lock
+			Select = {
+	"Grape",
+	"Loquat",
+	"Pepper",
+	"Cacao",
+	"Feijoa",
+	"Grand Tomato",
+	"Taco Fern",
+	"Beanstalk",
+	"Ember Lily",
+	"Sugar Apple",
+	"Burning Bud",
+	"Giant Pinecone",
+	"Elder Strawberry",
+	"Corn",
+	"Tomato"
+			},
+			Lock = {
+				"Fossilight",
+			}
+		}
+	},
+
+	["Seed Pack"] = {
+		Locked = {
+
+		}
+	},
+
+	Events = {
+		["Cook Event"] = {
+			Minimum_Money = 30_000_000, -- minimum money to start play this event
+		},
+		["Zen Event"] = {
+			["Restocking"] = { -- Minimumthing to restock
+				Max_Restocks_Price = 50_000_000,
+				Minimum_Money = 10_000_000,
+				Minimum_Chi = 200
+			},
+			["Doing"] = {
+				Minimum_Money = 30_000_000, -- minimum money to start play this event
+				First_Upgrade_Tree = 4,
+				Maximum_Chi = 250,
+
+				-- // thing to skip doing
+				Skip_Fox = false, -- Skip The Middle Fox Trade (Corrupted Kitsune)
+				Skip_Corrupted_OldMan = false, -- Skip The OldMan Trade (Kodama)
+				-- If u need to skip Tranquill OldMan Set "First Upgrade Tree" To 0 and Max Chi To 99999
+			}
+		},
+		["Traveling Shop"] = {
+			"Bee Egg",
+		},
+		Craft = {
+			"Ancient Seed Pack",
+			"Anti Bee Egg",
+			"Primal Egg",
+		},
+		Shop = {
+			"Zen Egg",
+			"Zen Seed Pack",
+			"Spiked Mango",
+			"Pet Shard Tranquil",
+			"Pet Shard Corrupted",
+			"Koi",
+			"Soft Sunshine",
+			"Sakura Bush",
+			"Raiju",
+		},
+		Start_Do_Honey = 2_000_000 -- start trade fruit for honey at money
+	},
+
+	Gear = {
+		Buy = { 
+			"Master Sprinkler",
+			"Godly Sprinkler",
+			"Advanced Sprinkler",
+			"Basic Sprinkler",
+			"Lightning Rod",
+			"Level Up Lollipop",
+			"Medium Treat",
+			"Medium Toy",
+			"Grandmaster Sprinkler",
+			"Trading Ticket",
+		},
+		Lock = {
+			"Lightning Rod",
+		},
+	},
+
+	Eggs = {
+		Place = {
+			"Gourmet Egg",
+			"Zen Egg",
+			"Paradise Egg",
+			"Common Summer Egg",
+		},
+		Buy = {
+			"Paradise Egg",
+			"Rare Summer Egg",
+			"Bug Egg",
+			"Mythical Egg",
+			"Common Summer Egg",
+		}
+	},
+
+	Pets = {
+		["Start Delete Pet At"] = 55,
+		["Upgrade Slot"] = {
+			["Pet"] = {
+				["Starfish"] = { 8, 76, 1, true }, -- the "true" on the last is auto equip (use for like only need to use for upgrade pet)
+			},
+			["Limit Upgrade"] = 5,
+			["Equip When Done"] = {
+				["Capybara"] = { 2, 100, 1 }, -- 5 on the first mean equip only 5 | pet , 100 mean equip only level pet lower than 100 | the one on the last is priority it will ues first if possible 
+				["Starfish"] = { 6, 75, 2 },
+			},
+		},
+		Favorite_LockedPet = true,
+		Locked_Pet_Age = 60, -- pet that age > 60 will lock
+		Locked = {
+			"French Fry Ferret",
+			"Spaghetti Sloth",
+			"Corrupted Kitsune",
+			"Kitsune",
+			"Capybara",
+			"Dragonfly",
+			"Mimic Octopus",
+			"Red Fox",
+			["Starfish"] = 40,
+		},
+		LockPet_Weight = 7, -- if Weight >= 10 they will locked
+		Instant_Sell = {
+			"Shiba Inu",
+			"Nihonzaru",
+			"Seagull",
+			"Peacock",
+			"Tanchozuru",
+			"Ostrich",
+			"Tanuki",
+			"Bagel Bunny",
+			"Kodama",
+			"Sushi Bear",
+			"Kappa",
+			"Scarlet Macaw",
+			"Crab",
+			"Pancake Mole",
+	},
+
+	Webhook = {
+		UrlPet = "https://discord.com/api/webhooks/1397396450404728934/a13XJ-ZPxjnQedqOxBTjCiB5bSiICrOGuJ6Z7z-lMVLX3DYH-hRPJBK-qU3cxgjGBIJH",
+		UrlSeed = "https://discord.com/api/webhooks/1397396450404728934/a13XJ-ZPxjnQedqOxBTjCiB5bSiICrOGuJ6Z7z-lMVLX3DYH-hRPJBK-qU3cxgjGBIJH",
+		PcName = "pc",
+
+		Noti = {
+			Seeds = {
+				"Sunflower",
+			},
+			SeedPack = {
+				"Idk"
+			},
+			Pets = {
+				"French Fry Ferret",
+				"Kitsune",
+			},
+			Pet_Weight_Noti = true,
+		}
+	},
+}
+License = "d4XPAyKydjPUvY8DQyf989Z0bDezqJbu"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
